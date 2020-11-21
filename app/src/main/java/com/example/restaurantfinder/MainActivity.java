60d5392f192823b46fa62d997c4262078bfbe007
kaboombrie <debrie2884@gmail.com>
2020-11-21 16:31:24 -0500

    Added navigation bar and maps

--- app/src/main/java/com/example/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/example/restaurantfinder/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.restaurantfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 0;
+    private static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            Log.d(TAG, "getLocation: permissions granted");
+        }
+    }
+
 }
